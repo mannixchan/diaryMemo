4ab62c0d79cc3ce0bcdddb6ef571acb0a6c676d4
mannixchan <51387089+mannixchan@users.noreply.github.com>
2022-06-29 12:09:38 +0800

    ok

---
+++ src/main/java/gnim/top/diarymemo/DiaryMemoApplication.java
@@ -0,0 +1,13 @@
+package gnim.top.diarymemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryMemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryMemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gnim/top/diarymemo/DiaryMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package gnim.top.diarymemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryMemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
