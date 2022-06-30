0bf7c4bca3188de788ed894fac63d2442d7b20e2
mannixchan <51387089+mannixchan@users.noreply.github.com>
2022-06-30 22:51:10 +0800

    test Controller

--- src/main/java/gnim/top/diarymemo/controller/PassportController.java
+++ src/main/java/gnim/top/diarymemo/controller/PassportController.java
@@ -1,10 +1,16 @@
 package gnim.top.diarymemo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/passport")
 public class PassportController {
+    @PostMapping("/signin")
+    public String login() {
+        System.out.println("i'm loginning");
+        return ("jojo");
+    }
     @GetMapping("/hello")
     public Integer hello() {
         System.out.println("hello");
